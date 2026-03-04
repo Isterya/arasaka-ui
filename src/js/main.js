f72b714d1c681d40750723f3be04b24e07d928eb
isterya <vash.vybor.1@gmail.com>
2026-03-04 20:36:41 +0100

    WIP: end of day

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,14 @@ import $ from './lib/lib';
 //     $(this).toggleClass('active');
 // })
 
-$('button').toggleAttr('disabled');
+$('button').on('click', function() {
+    $('div').eq(2).toggleClass('active')
+})
+
+$('div').click(function() {
+    console.log($(this).index())
+})
+
+// console.log($('div').eq(2).find('.some'));
+// console.log($('.some').closest('.findme'))
+console.log($('.more').eq(0).siblings())
