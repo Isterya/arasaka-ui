b241ed421a2c863ad881e11ec1eeedfbfe8434e5
isterya <vash.vybor.1@gmail.com>
2026-03-13 18:53:14 +0100

    feat(effects): implement animationOverTime method

--- src/js/lib/modules/effects.js
+++ src/js/lib/modules/effects.js
@@ -10,5 +10,17 @@ $.prototype.animationOverTime = function (dur, cb, fin) {
 
     let timeElapsed = time - timeStart;
     let complection = Math.min(timeElapsed / dur, 1);
+
+    cb(complection);
+
+    if (timeElapsed < dur) {
+      requestAnimationFrame(_animateOverTime);
+    } else {
+      if (typeof fin === 'function') {
+        fin();
+      }
+    }
   }
+
+  return _animateOverTime();
 };
