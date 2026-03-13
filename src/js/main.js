574d5cece09572b71ca1b0c603051f5fcccff62b
isterya <vash.vybor.1@gmail.com>
2026-03-13 09:34:03 +0100

    chore(devs): add prettier

--- src/js/main.js
+++ src/js/main.js
@@ -4,14 +4,16 @@ import $ from './lib/lib';
 //     $(this).toggleClass('active');
 // })
 
-$('button').on('click', function() {
-    $('div').eq(2).toggleClass('active')
-})
+// $('button').on('click', function() {
+//     $('div').eq(2).toggleClass('active')
+// })
+//
+// $('div').click(function() {
+//     console.log($(this).index())
+// })
+//
+// // console.log($('div').eq(2).find('.some'));
+// // console.log($('.some').closest('.findme'))
+// console.log($('.more').eq(0).siblings())
 
-$('div').click(function() {
-    console.log($(this).index())
-})
-
-// console.log($('div').eq(2).find('.some'));
-// console.log($('.some').closest('.findme'))
-console.log($('.more').eq(0).siblings())
+$('.active').html(`<div>${$('button').html()}</div>`);
