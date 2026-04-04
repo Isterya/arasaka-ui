34369e59b8b45dc6b984589b67685d0d7938d763
isterya <vash.vybor.1@gmail.com>
2026-04-04 22:17:54 +0200

    fix(modal)!: refactor code and fix bug where an accumulation of created modal windows is causing performance issues

--- src/js/lib/components/modal.js
+++ src/js/lib/components/modal.js
@@ -1,6 +1,6 @@
 import $ from '../core';
 
-$.prototype.modal = function () {
+$.prototype.modal = function (created) {
   for (let i = 0; i < this.length; i++) {
     const target = this[i].getAttribute('data-target');
 
@@ -10,22 +10,30 @@ $.prototype.modal = function () {
 
       document.body.style.overflow = 'hidden';
     });
-  }
 
-  const closeElements = document.querySelectorAll('[data-close]');
-  closeElements.forEach((elem) => {
-    $(elem).click(() => {
-      $('.modal').fadeOut(500);
-      document.body.style.overflow = '';
+    const closeElements = document.querySelectorAll(`${target} [data-close]`);
+    closeElements.forEach((elem) => {
+      $(elem).click(() => {
+        $(target).fadeOut(500);
+        document.body.style.overflow = '';
+
+        if (created) {
+          document.querySelector(target).remove();
+        }
+      });
     });
-  });
 
-  $('.modal').click((e) => {
-    if (e.target.classList.contains('modal')) {
-      $('.modal').fadeOut(500);
-      document.body.style.overflow = '';
-    }
-  });
+    $(target).click((e) => {
+      if (e.target.classList.contains('modal')) {
+        $(target).fadeOut(500);
+        document.body.style.overflow = '';
+
+        if (created) {
+          document.querySelector(target).remove();
+        }
+      }
+    });
+  }
 };
 
 $('[data-toggle="modal"]').modal();
@@ -83,7 +91,7 @@ $.prototype.createModal = function ({ text, btns } = {}) {
     modal.querySelector('.modal-footer').append(...buttons);
     document.body.appendChild(modal);
 
-    $(this[i]).modal();
+    $(this[i]).modal(true);
     $(this[i].getAttribute('data-target')).fadeIn(500);
   }
 };
