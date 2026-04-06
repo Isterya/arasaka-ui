220c998611eee335bd3cd1903a7aa1b9f09880f5
isterya <vash.vybor.1@gmail.com>
2026-04-06 13:55:36 +0200

    fix(accordion): fix incorrect accordion closing

--- src/js/lib/components/accordion.js
+++ src/js/lib/components/accordion.js
@@ -3,13 +3,17 @@ import $ from '../core';
 $.prototype.accordion = function () {
   for (let i = 0; i < this.length; i++) {
     $(this[i]).on('click', () => {
+      const isActive = this[i].classList.contains('accordion-heading--active');
+
       for (let j = 0; j < this.length; j++) {
         $(this[j]).removeClass('accordion-heading--active');
         this[j].nextElementSibling.classList.remove('accordion-block--open');
       }
 
-      $(this[i]).toggleClass('accordion-heading--active');
-      this[i].nextElementSibling.classList.toggle('accordion-block--open');
+      if (!isActive) {
+        $(this[i]).addClass('accordion-heading--active');
+        this[i].nextElementSibling.classList.add('accordion-block--open');
+      }
     });
   }
 };
