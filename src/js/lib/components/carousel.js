e988d08bd4b50802f8e1f3ad619f6b31fdfd304f
isterya <vash.vybor.1@gmail.com>
2026-04-07 14:29:47 +0200

    feat(carousel): add event listener to next button

--- src/js/lib/components/carousel.js
+++ src/js/lib/components/carousel.js
@@ -13,6 +13,18 @@ $.prototype.carousel = function () {
     slides.forEach((slide) => {
       slide.style.width = width;
     });
+
+    let offset = 0;
+
+    $(this[i].querySelector('[data-slide="next"]')).click(() => {
+      if (offset === +width.replace(/\D/g, '') * (slides.length - 1)) {
+        offset = 0;
+      } else {
+        offset += +width.replace(/\D/g, '');
+      }
+
+      slidesField.style.transform = `translateX(-${offset}px)`;
+    });
   }
 };
 
